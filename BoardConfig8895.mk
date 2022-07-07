f17af9e945834922dbefab92a7a1e9f374dd9ef8
Jan Altensen <info@stricted.net>
2022-07-07 14:55:39 +0200

    config: add basic configurations
    Change-Id: I6915f603a9194f6349b92d0a9780660e291b1b3d

---
+++ BoardConfig8895.mk
@@ -0,0 +1,63 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+TARGET_LINUX_KERNEL_VERSION := 4.4
+
+include hardware/samsung_slsi-linaro/config/BoardConfigCommon.mk
+
+USE_OPENGL_RENDERER := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+BOARD_USES_EXYNOS5_COMMON_GRALLOC := true
+BOARD_USES_EXYNOS_GRALLOC_VERSION := 1
+BOARD_USES_ALIGN_RESTRICTION := true
+BOARD_USES_GRALLOC_ION_SYNC := true
+
+# Samsung OpenMAX Video
+BOARD_USE_DMA_BUF := true
+BOARD_USE_NON_CACHED_GRAPHICBUFFER := true
+BOARD_USE_GSC_RGB_ENCODER := true
+BOARD_USE_CSC_HW := false
+BOARD_USE_S3D_SUPPORT := false
+BOARD_USE_DEINTERLACING_SUPPORT := true
+BOARD_USE_HEVCENC_SUPPORT := true
+BOARD_USE_HEVC_HWIP := false
+BOARD_USE_VP9DEC_SUPPORT := true
+BOARD_USE_VP9ENC_SUPPORT := true
+BOARD_USE_WFDENC_SUPPORT := false
+BOARD_USE_CUSTOM_COMPONENT_SUPPORT := false
+BOARD_USE_VIDEO_EXT_FOR_WFD_HDCP := true
+BOARD_USE_SINGLE_PLANE_IN_DRM := true
+BOARD_USE_WA_ION_BUF_REF := true
+
+# HWComposer
+BOARD_HWC_VERSION := libhwc2
+BOARD_HDMI_INCAPABLE := true
+BOARD_USES_HWC_SERVICES := false
+TARGET_USES_HWC2 := true
+HWC_SKIP_VALIDATE := true
+
+
+# SCALER
+BOARD_USES_DEFAULT_CSC_HW_SCALER := true
+BOARD_DEFAULT_CSC_HW_SCALER := 4
+BOARD_USES_SCALER_M2M1SHOT := true
+BOARD_HAS_SCALER_ALIGN_RESTRICTION := true
+
+# LIBHWJPEG
+TARGET_USES_UNIVERSAL_LIBHWJPEG := true
+
+# Acryl
+BOARD_LIBACRYL_DEFAULT_COMPOSITOR := fimg2d_8895
