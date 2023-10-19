2f584ad1839b0cee7c57df5f0ae0ee934c6978f3
K9100ii <k9100ii.ki@gmail.com>
2023-10-19 16:41:50 +0100

    config: Add configuration for the Exynos 7885 and 7904
    Of note is that BOARD_HWJPEG_ANDROID_VERSION needs to be set in device
    variant trees as different devices did have their last updates on
    different Android versions.
    
    Otherwise, no known issues with supported functionality.
    
    Change-Id: Icacbd3736192158f50be82b02c1e80b07fdc427c

---
+++ BoardConfig7885.mk
@@ -0,0 +1,63 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+TARGET_LINUX_KERNEL_VERSION := 4.4
+
+TARGET_SOC_BASE := exynos7885
+
+BOARD_USES_EXYNOS5_COMMON_GRALLOC := true
+BOARD_USES_EXYNOS_GRALLOC_VERSION := 3
+BOARD_USES_ALIGN_RESTRICTION := true
+BOARD_MFC_CHROMA_VALIGN := 32
+BOARD_USES_EXYNOS_DATASPACE_FEATURE := true
+
+# Samsung OpenMAX Video
+BOARD_USE_DMA_BUF := true
+BOARD_USE_NON_CACHED_GRAPHICBUFFER := true
+BOARD_USE_GSC_RGB_ENCODER := true
+BOARD_USE_CSC_HW := true
+BOARD_USE_S3D_SUPPORT := false
+BOARD_USE_DEINTERLACING_SUPPORT := true
+BOARD_USE_HEVCENC_SUPPORT := true
+BOARD_USE_HEVC_HWIP := false
+BOARD_USE_VP9DEC_SUPPORT := true
+BOARD_USE_VP9ENC_SUPPORT := false
+BOARD_USE_WFDENC_SUPPORT := false
+BOARD_USE_CUSTOM_COMPONENT_SUPPORT := true
+BOARD_USE_VIDEO_EXT_FOR_WFD_HDCP := true
+BOARD_USE_SINGLE_PLANE_IN_DRM := true
+BOARD_USE_WA_ION_BUF_REF := true
+
+# HWComposer
+BOARD_HWC_VERSION := hwc3
+TARGET_USES_HWC2 := true
+HWC_SKIP_VALIDATE := true
+BOARD_USES_HWC_SERVICES := false
+
+# SCALER
+BOARD_USES_DEFAULT_CSC_HW_SCALER := true
+BOARD_DEFAULT_CSC_HW_SCALER := 4
+BOARD_USES_SCALER_M2M1SHOT := true
+BOARD_HAS_SCALER_ALIGN_RESTRICTION := true
+
+# LIBHWJPEG
+TARGET_USES_UNIVERSAL_LIBHWJPEG := true
+
+# Acryl
+BOARD_LIBACRYL_DEFAULT_COMPOSITOR := fimg2d_8895
+BOARD_LIBACRYL_DEFAULT_SCALER := mscl_9810
+
+include hardware/samsung_slsi-linaro/config/BoardConfigCommon.mk
